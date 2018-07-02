32f8fbdcff05a70e37d3c2fd1d809cf939dcbc0f
Gustavo Moura <gus.moura@outlook.com>
2018-07-02 15:21:04 -0300

    Interface e botões ativos

--- app.py
+++ app.py
@@ -15,26 +15,46 @@ class AppWindow(QMainWindow):
 
 
     def inicializa_botoes(self):
+        # Menu
         self.ui.actionCr_ditos.triggered.connect(self.click_creditos)
         self.ui.actionInserir_Informa_es.triggered.connect(self.click_inserir)
         self.ui.actionCarregar_Pre_Defini_es.triggered.connect(self.click_predefinir)
         self.ui.actionRodar_Execu_o.triggered.connect(self.click_MRP)
 
+        # Cadastrar
+        self.ui.btn_cadastrar.clicked.connect(self.click_cadastrar)
+
 
 
     # Declaração de Funções
 
+    # Menu
     def click_creditos(self):
+        # Exibe créditos
         QMessageBox.about(self.ui.stackedWidget, "Créditos", "Sistema desenvolvido por: Yago Pessoa, Gustavo Moura, Thais Nobre e Alef Segura no âmbito da disciplina de Modelagem da Produção - ICMC - USP - julho de 2018")
     
     def click_inserir(self):
+        # abrir a janela de cadastro de item
         pass
     
     def click_predefinir(self):
-        pass
+        # ao inves de ter que cadastrar todos os itens, já pré-inserir uma quantidade legal suficiente para
+        # realizar os testes  (inserir tudo de object.py).
+
+        # Mensagem de informação de sucesso
+        QMessageBox.about(self.ui.stackedWidget, "Pré-definido", "Estado do sistema pronto para rodar o MRP.")
+    
     
     def click_MRP(self):
+        # roda o MRP
         pass
+
+    # Cadastrar
+    def click_cadastrar(self):
+        # realizar a verificação: se o item já foi cadastrado, perguntar se quer substituir as informações dele
+        # com as informações inseridas agora
+        pass
+
 
 
 
--- object.py
+++ object.py
@@ -1,4 +1,7 @@
 
+
+
+# Classe que organiza os itens que serão utilizados no MRP
 class Item():
 	def __init__(self, codigo, nome, nivel, tr, lote):
 		self.codigo = codigo	# Código do item 
@@ -9,6 +12,7 @@ class Item():
 		self.estoque = estoque
 
 
+# O MRP em si. Um para cada item
 class Item_MRP():
 	# Trabalhando com MRP de 12 semanas
 
