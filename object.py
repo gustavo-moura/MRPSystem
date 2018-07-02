e5fcbac2221eb081b6797cc94d32291e18d95ef0
Gustavo Moura <gus.moura@outlook.com>
2018-07-02 17:54:54 -0300

    Tentativas de executar o MRP

--- object.py
+++ object.py
@@ -1,6 +1,3 @@
-
-
-
 # Classe que organiza os itens que serão utilizados no MRP
 class Item():
 	def __init__(self, codigo, nome, nivel, tr, lote, emin, eatual):
@@ -20,12 +17,35 @@ class Item_MRP():
 	def __init__(self, item):
 		self.item = item
 
+	'''
+	NP: Necessidades Prevista	[0..12]
+	RP: Recebimento Programado	[1..12]
+	ED: Estoque Disponível		[1..12]
+	NL: Necessidade Líquida		[1..12]
+	LP: Liberação de Pedido		[1..12]
+	'''
+
+	# Receber NP de cada item pra cada semana
+
+	# Definir o estoque mínimo
+	ED[0] = self.item.eatual
+
+	for i in range(1, 12):
+
+		if(NP[i]>0):
+			NL[i] = NP[i] - ED[i-1]
+
+			if(NL[i]>0):
+				teste = i-self.item.tr
+
+				if(teste<0):
+					# Erro: O pedido de compra do <item> deve ser realizado |<teste>| dias antes do início da execução do plano
+
+				else:
+					LP[i-self.item.tr] = NL[i]
+
+		ED[i] = ED[i-1] + RP[i] - NP[i]
 
 
-	'''
-	NB: Necessidades Brutas
-	RP: Recebimento Programado
-	ED: Estoque Disponível
-	NL: Necessidade Líquida
-	LP: Liberação de Pedido
-	'''
+
+
